13b57828ca14031236565c48990262c68ce2e6f5
league <karinabriellen@gmail.com>
2024-02-14 20:09:11 -0800

    the bfg

---
+++ src/_01_chuckle_clicker/CookieClicker.java
@@ -0,0 +1,17 @@
+package _01_chuckle_clicker;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class CookieClicker {
+	public void makeButtons() {
+		JFrame arcana = new JFrame();
+		arcana.setVisible(true);
+	    JButton button = new JButton();
+	    arcana.add(button);
+	    arcana.pack();
+	}
+	static public void main(String[] args) {
+		
+	}
+}
