965b41b8950be171a523756cad772be5c91ebe62
jaswi <jaswi@LAPTOP-OK3U2FM1>
2024-02-21 18:21:41 -0800

    sigh

--- src/_01_chuckle_clicker/CookieClicker.java
+++ src/_01_chuckle_clicker/CookieClicker.java
@@ -1,15 +1,26 @@
 package _01_chuckle_clicker;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class CookieClicker {
+public class CookieClicker implements ActionListener {
 	public void makeButtons() {
 		JFrame arcana = new JFrame();
 		arcana.setVisible(true);
 	    JButton button = new JButton();
 	    arcana.add(button);
+	    JButton mushroom = new JButton();
+	    arcana.add(mushroom);
 	    arcana.pack();
+	    button.addActionListener(this);
+	    mushroom.addActionListener(this);
+	    
+	    
+	}
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("hi");
 	}
 	static public void main(String[] args) {
 		
